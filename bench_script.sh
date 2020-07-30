a70793627945c0306d6891c2361d0e0af06e263f
Leaderboard <x010@LAPTOP-2UR547VR.localdomain>
2020-07-30 13:45:50 +0300

    energyscript

--- bench_script.sh
+++ bench_script.sh
@@ -1,8 +1,7 @@
 # go to the beebs/src directory
 cd src
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/{}.txt ./{}" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/{}.txt ./{}" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/{}.txt ./{}" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/{}.txt ./{}" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/{}.txt ./{}" \;
-
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/time/{}.txt ./{}" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/time/{}.txt ./{}" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/time/{}.txt ./{}" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/time/{}.txt ./{}" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && /usr/bin/time -ao ../../results/time/{}.txt ./{}" \;
---
+++ energy_script.sh
@@ -0,0 +1,7 @@
+# go to the URAS (beebs)/src directory
+cd src
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt " \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
