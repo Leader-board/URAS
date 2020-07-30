c58e10e9fda687dfe39933beed247913b6106de3
Leader-board <dm282@st-andrews.ac.uk>
2020-07-30 12:04:32 +0100

    update

--- energy_script.sh
+++ energy_script.sh
@@ -1,7 +1,8 @@
 # go to the URAS (beebs)/src directory
 cd src
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt " \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ./../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ./../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ./../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ./../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && pwd && ./../../energy/AppPowerMeter ./{} >> ../../results/energy/{}.txt" \;
+cd ../
