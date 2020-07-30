166c241b5df1babdfde4a6b071a9335ff82aeaa8
Leaderboard <x010@LAPTOP-2UR547VR.localdomain>
2020-07-30 12:22:22 +0300

    update

--- bench_script.sh
+++ bench_script.sh
@@ -3,7 +3,8 @@ cd src
 for d in *
 do
     cd "$d"
-    \time -o ../results/res_"$d".txt ./"$d"
-    cd ../
+#    \time -o ../results/res_"$d".txt ./"$d"
+    { time ./$d ; } 2> ../results/$d.txt
+#    cd ../
 done
 cd ../
