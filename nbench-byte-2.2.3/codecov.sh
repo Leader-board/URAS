b2096792ac623b9b71fc34f13523327a8986a151
Leaderboard <vishnu9145@hotmail.com>
2020-09-01 13:19:13 +0300

    scripting

---
+++ nbench-byte-2.2.3/codecov.sh
@@ -0,0 +1,32 @@
+./nbench 2
+gcov nbench1.c
+mkdir mkdir codecoverage/bench2
+mv *.gcov codecoverage/bench2
+./nbench 3
+gcov nbench1.c
+mkdir mkdir codecoverage/bench3
+mv *.gcov codecoverage/bench3
+./nbench 4
+gcov nbench1.c
+mkdir mkdir codecoverage/bench4
+mv *.gcov codecoverage/bench4
+./nbench 5
+gcov nbench1.c
+mkdir mkdir codecoverage/bench5
+mv *.gcov codecoverage/bench5
+./nbench 6
+gcov nbench1.c
+mkdir mkdir codecoverage/bench6
+mv *.gcov codecoverage/bench6
+./nbench 7
+gcov nbench1.c
+mkdir mkdir codecoverage/bench7
+mv *.gcov codecoverage/bench7
+./nbench 8
+gcov nbench1.c
+mkdir mkdir codecoverage/bench8
+mv *.gcov codecoverage/bench8
+./nbench 9
+gcov nbench1.c
+mkdir mkdir codecoverage/bench9
+mv *.gcov codecoverage/bench9
